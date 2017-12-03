e8668811227d1ce2c2a46414d692315b5236676f
Hein Bekker <hein@siyavula.com>
2017-12-03 16:00:07 +0200

    Add script to build ttfautohint from source

---
+++ scripts/install-ttfautohint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This scripts builds ttfautohint from source. This is required in Ubuntu 15-16
+# because ttfautohint 0.97-x throws `malloc(): memory corruption` error.
+
+sudo apt-get purge -y -qq libharfbuzz* ttfautohint
+
+sudo apt-get install -y -qq autoconf automake bison flex git pkg-config gcc g++ libglib2.0-dev libcairo2-dev libtool libc6 libfreetype6 libgcc1 libqtcore4 libqtgui4 libstdc++6 libqt4-dev libfreetype6-dev pandoc pkg-config texlive-xetex help2man
+
+cd ~
+wget https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.4.2.tar.bz2
+tar -xjf harfbuzz-1.4.2.tar.bz2
+rm harfbuzz-1.4.2.tar.bz2
+cd harfbuzz-1.4.2
+./configure
+make
+sudo make install
+
+cd ~
+git clone http://repo.or.cz/ttfautohint.git
+cd ttfautohint
+./bootstrap
+./configure --with-doc=no --with-qt=no
+make
+sudo make install
