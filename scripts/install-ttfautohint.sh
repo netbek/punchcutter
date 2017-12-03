50dda33bce4b7f7d95f3759511cabc6993cbefde
Hein Bekker <hein@siyavula.com>
2017-12-03 16:14:31 +0200

    Make ttfautohint install script more verbose

--- scripts/install-ttfautohint.sh
+++ scripts/install-ttfautohint.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # This scripts builds ttfautohint from source. This is required in Ubuntu 15-16
 # because ttfautohint 0.97-x throws `malloc(): memory corruption` error.
 
-sudo apt-get purge -y -qq libharfbuzz* ttfautohint
+sudo apt-get purge -y libharfbuzz* ttfautohint
 
-sudo apt-get install -y -qq autoconf automake bison flex git pkg-config gcc g++ libglib2.0-dev libcairo2-dev libtool libc6 libfreetype6 libgcc1 libqtcore4 libqtgui4 libstdc++6 libqt4-dev libfreetype6-dev pandoc pkg-config texlive-xetex help2man
+sudo apt-get install -y autoconf automake bison flex git pkg-config gcc g++ libglib2.0-dev libcairo2-dev libtool libc6 libfreetype6 libgcc1 libqtcore4 libqtgui4 libstdc++6 libqt4-dev libfreetype6-dev pandoc pkg-config texlive-xetex help2man
 
 cd ~
 wget https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.4.2.tar.bz2
